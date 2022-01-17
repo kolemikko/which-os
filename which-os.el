6f43f18ce9aace54860180fc292b1aec64a78297
kolemikko <mikko.kolehmainen@tuta.io>
2022-01-17 21:11:49 +0200

    Added functions for lsw and generic package header

--- which-os.el
+++ which-os.el
@@ -1,3 +1,12 @@
+;;; which-os.el --- helper functions for cross platform emacs configuration
+
+;; Author: Mikko Kolehmainen
+;; Version: 0.1
+;; Keywords: cross platform, system-type
+;; URL: https://github.com/kolemikko/which-os
+
+;;; Commentary:
+;; This package includes selection of helper functions to work with cross platform emacs configurations
 
 (defun is-linux()
   (when (equal system-type 'gnu/linux)
@@ -11,5 +20,11 @@
   (when (equal system-type 'windows-nt)
     t))
 
+(defun is-lsw()
+  (when (equal system-type 'gnu/linux)
+      (string-suffix-p "Microsoft" (string-trim (shell-command-to-string "uname -a"))
+		       t)))
+
 (defun is-android()
       (string-suffix-p "Android" (string-trim (shell-command-to-string "uname -a"))))
+
